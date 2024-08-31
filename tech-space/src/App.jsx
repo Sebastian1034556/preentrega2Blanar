e9758b234b875bdd777e38fb3a1a1befe089fcc0
sebastian <sebastianblanar@gmail.com>
2024-08-31 09:00:25 -0600

    fetching

--- tech-space/src/App.jsx
+++ tech-space/src/App.jsx
@@ -1,190 +1,86 @@
 import './App.css'
 import { NavBar } from './components/NavBar'
 import { HomeComponent } from './components/HomeView/HomeComponent'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ItemDetail } from './components/ItemDetail'
 import { Link } from 'react-router-dom'
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [itemList,setItemList] = useState([]);
-  const siteId = 'MLA'; 
-  const categoryId = 'MLA1051'; 
+  const [itemList, setItemList] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const siteId = 'MLA';
+  const categoryId = 'MLA1051';
   const url = `https://api.mercadolibre.com/sites/${siteId}/search?category=${categoryId}`;
-  const ML_CATEGORIES_LIST = [
-    {
-        "id": "MLA5725",
-        "name": "Accesorios para Vehículos"
-    },
-    {
-        "id": "MLA1512",
-        "name": "Agro"
-    },
-    {
-        "id": "MLA1403",
-        "name": "Alimentos y Bebidas"
-    },
-    {
-        "id": "MLA1071",
-        "name": "Animales y Mascotas"
-    },
-    {
-        "id": "MLA1367",
-        "name": "Antigüedades y Colecciones"
-    },
-    {
-        "id": "MLA1368",
-        "name": "Arte, Librería y Mercería"
-    },
-    {
-        "id": "MLA1743",
-        "name": "Autos, Motos y Otros"
-    },
-    {
-        "id": "MLA1384",
-        "name": "Bebés"
-    },
-    {
-        "id": "MLA1246",
-        "name": "Belleza y Cuidado Personal"
-    },
-    {
-        "id": "MLA1039",
-        "name": "Cámaras y Accesorios"
-    },
-    {
-        "id": "MLA1051",
-        "name": "Celulares y Teléfonos"
-    },
-    {
-        "id": "MLA1648",
-        "name": "Computación"
-    },
-    {
-        "id": "MLA1144",
-        "name": "Consolas y Videojuegos"
-    },
-    {
-        "id": "MLA1500",
-        "name": "Construcción"
-    },
-    {
-        "id": "MLA1276",
-        "name": "Deportes y Fitness"
-    },
-    {
-        "id": "MLA5726",
-        "name": "Electrodomésticos y Aires Ac."
-    },
-    {
-        "id": "MLA1000",
-        "name": "Electrónica, Audio y Video"
-    },
-    {
-        "id": "MLA2547",
-        "name": "Entradas para Eventos"
-    },
-    {
-        "id": "MLA407134",
-        "name": "Herramientas"
-    },
-    {
-        "id": "MLA1574",
-        "name": "Hogar, Muebles y Jardín"
-    },
-    {
-        "id": "MLA1499",
-        "name": "Industrias y Oficinas"
-    },
-    {
-        "id": "MLA1459",
-        "name": "Inmuebles"
-    },
-    {
-        "id": "MLA1182",
-        "name": "Instrumentos Musicales"
-    },
-    {
-        "id": "MLA3937",
-        "name": "Joyas y Relojes"
-    },
-    {
-        "id": "MLA1132",
-        "name": "Juegos y Juguetes"
-    },
-    {
-        "id": "MLA3025",
-        "name": "Libros, Revistas y Comics"
-    },
-    {
-        "id": "MLA1168",
-        "name": "Música, Películas y Series"
-    },
-    {
-        "id": "MLA1430",
-        "name": "Ropa y Accesorios"
-    },
-    {
-        "id": "MLA409431",
-        "name": "Salud y Equipamiento Médico"
-    },
-    {
-        "id": "MLA1540",
-        "name": "Servicios"
-    },
-    {
-        "id": "MLA9304",
-        "name": "Souvenirs, Cotillón y Fiestas"
-    },
-    {
-        "id": "MLA1953",
-        "name": "Otras categorías"
-    }
-  ]
-  const renameCategories = (catId,categories) => categories.find(cat => cat.id === catId)
+  const CATEGORY_LIST_URL = `https://api.mercadolibre.com/sites/${siteId}/categories`
 
-  useEffect(()=>{
-    fetch(url)
-      .then(res => res.json())
+  useEffect(() => {
+    async function getCategories() {
+      const response = await fetch(CATEGORY_LIST_URL);
+      const data = await response.json();
+      console.table(data)
+      setCategories(data);
+    }
+    getCategories();
+  }, []);
+
+  useEffect(() => {
+    if (categories.length === 0) return;
+  
+    async function getItems() {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    }
+  
+    getItems()
       .then(data => {
-        
+        const renameCategories = (catId, categories) => {
+          const category = categories.find(c => c.id === catId);
+          // El error está acá. category es undefined.
+          const categoryName = category ? category.name : 'Sin categoría';
+          return categoryName;
+        }
+  
         const items = data.results.map(item => {
           return {
             id: item.id,
             title: item.title,
             price: item.price,
             image: item.thumbnail,
-            category: renameCategories(item.category_id,ML_CATEGORIES_LIST).name
+            category: renameCategories(item.category_id, categories)
           }
-        })
-        setItemList(items)
+        });
+        setItemList(items);
       })
-      .catch(err => console.log(err))
-  },[])
+      .catch(err => console.log(err));
+  
+  }, [categories]);
+  
 
   return (
     <>
       <BrowserRouter>
         <header className="bg-gray-600 p-4 flex justify-between items-center">
           <Link to="/">
-            <img src="/images/tech-space-logo.png" alt="Logo de Techspace" className=" h-24 mr-5"/>
+            <img src="/images/tech-space-logo.png" alt="Logo de Techspace" className="h-24 mr-5" />
           </Link>
-          <NavBar/>
+          <NavBar />
         </header>
         <main>
-        {itemList.length > 0 ? (
-          <Routes>
-            <Route path="/" element={<HomeComponent itemList={itemList} />} />
-            <Route path="/category/:id" element={<HomeComponent itemList={itemList} />} />
-            <Route path="/item/:itemId" element={<ItemDetail itemList={itemList} />} />
-          </Routes>
-        ) : (
-          <p className='text-center'>Cargando...</p>  
-        )}
+          {itemList.length > 0 ? (
+            <Routes>
+              <Route path="/" element={<HomeComponent itemList={itemList} />} />
+              <Route path="/category/:id" element={<HomeComponent itemList={itemList} />} />
+              <Route path="/item/:itemId" element={<ItemDetail itemList={itemList} />} />
+            </Routes>
+          ) : (
+            <p className='text-center'>Cargando...</p>
+          )}
         </main>
       </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
